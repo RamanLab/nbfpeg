616603448bff87e028de4447f8160fe3fa0d69b3
karthik1290 <karthikalam12.90@gmail.com>
2018-09-22 00:44:33 +0530

    codes

---
+++ leaveoneout/dothis.sh
@@ -0,0 +1,54 @@
+sed "s/trainfiles=/trainfiles=[\'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'AC\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMAC.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'b\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMb.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'BP\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMBP.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'BSU\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMBSU.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'BTH_\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMBTH_.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'BT_\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMBT_.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'CCNA_\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMCCNA_.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'Cj\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMCj.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'FT\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMFT.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'HI\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMHI.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'HP\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMHP.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'MG_\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMMG_.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'MY\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMMY.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'PA14_\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMPA14_.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'PA\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMPA.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'PGN_\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMPGN_.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'Rv\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMRv.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'SA\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMSA.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'SAOU\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMSAOU.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'SO\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMSO.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'spr\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMspr.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'SSA_\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'Spy\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMSpy.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'STM\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'SSA_\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMSSA_.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'Swit_\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'STM\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMSTM.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'t\', \'VC\']/
+	s/testfiles=/testfiles=[\'Swit_\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMSwit_.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'VC\']/
+	s/testfiles=/testfiles=[\'t\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMt.py
+sed "s/trainfiles=/trainfiles=[\'AC\', \'b\', \'BP\', \'BSU\', \'BTH_\', \'BT_\', \'CCNA_\', \'Cj\', \'FT\', \'HI\', \'HP\', \'MG_\', \'MY\', \'PA14_\', \'PA\', \'PGN_\', \'Rv\', \'SA\', \'SAOU\', \'SO\', \'spr\', \'Spy\', \'SSA_\', \'STM\', \'Swit_\', \'t\']/
+	s/testfiles=/testfiles=[\'VC\']/" leaveoneoutnetworkacrossSVMAC_1.py > leaveoneoutnetworkacrossSVMVC.py
---
+++ leaveoneout/loo.sh
@@ -0,0 +1,2 @@
+echo "python leaveoneoutnetworkacrossSVM"$1".py allfeatsorted.txt > loo597"$1".txt"
+echo "cp loo597"$1".txt /data/karthik/loo597/"
